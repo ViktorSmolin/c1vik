b8f1642b2eb6a31b46e96533fab90e4fa658da24
Viktor <vsmolin8@mail.ru>
2021-03-23 22:55:20 +0700

    fitsr commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// const result = confirm ("Are you here?");
+
+// console.log (result);
+let arr = [];
+
+arr[0] = prompt ("Как ваше имя?", "");
+
+arr[1] = prompt ("Как ваша фамилия?", "" );
+
+arr[2] = prompt ("Сколько вам лет?", "" );
+
+document.write(arr);
